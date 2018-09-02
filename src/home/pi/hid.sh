78ce48d829ffb4916231a78889f8db66ea763abe
Inusha Hapuarachchi <inusha@outlook.com>
2018-09-02 15:02:47 +0100

    update hid.sh for mouse emulation

--- src/home/pi/hid.sh
+++ src/home/pi/hid.sh
@@ -7,20 +7,26 @@ modprobe libcomposite
 cd /sys/kernel/config/usb_gadget/
 mkdir -p g1
 cd g1
-echo 0x1d6b > idVendor # Linux Foundation
-echo 0x0104 > idProduct # Multifunction Composite Gadget
-echo 0x0100 > bcdDevice # v1.0.0
-echo 0x0200 > bcdUSB # USB2
+echo 0xc1ce > idVendo
+echo 0xbaa3 > idProduc
+echo 0x0100 > bcdDevice
+echo 0x0200 > bcdUSB
+
+# composite class / subclass / proto (needs single configuration)
+echo 0xEF > bDeviceClass
+echo 0x02 > bDeviceSubClass
+echo 0x01 > bDeviceProtocol
+
 mkdir -p strings/0x409
 echo "deadbeef01234567890" > strings/0x409/serialnumber
 echo "example.com" > strings/0x409/manufacturer
-echo "Generic USB Keyboard" > strings/0x409/product
+echo "Generic USB Mouse" > strings/0x409/product
 N="usb0"
 mkdir -p functions/hid.$N
-echo 1 > functions/hid.usb0/protocol
-echo 1 > functions/hid.usb0/subclass
-echo 8 > functions/hid.usb0/report_length
-echo -ne \\x05\\x01\\x09\\x06\\xa1\\x01\\x05\\x07\\x19\\xe0\\x29\\xe7\\x15\\x00\\x25\\x01\\x75\\x01\\x95\\x08\\x81\\x02\\x95\\x01\\x75\\x08\\x81\\x03\\x95\\x05\\x75\\x01\\x05\\x08\\x19\\x01\\x29\\x05\\x91\\x02\\x95\\x01\\x75\\x03\\x91\\x03\\x95\\x06\\x75\\x08\\x15\\x00\\x25\\x65\\x05\\x07\\x19\\x00\\x29\\x65\\x81\\x00\\xc0 > functions/hid.usb0/report_desc
+echo 2 > functions/hid.$N/protocol
+echo 1 > functions/hid.$N/subclass
+echo 6 > functions/hid.$N/report_length
+echo -ne \\x05\\x01\\x09\\x02\\xa1\\x01\\x09\\x01\\xa1\\x00\\x05\\x09\\x19\\x01\\x29\\x03\\x15\\x00\\x25\\x01\\x95\\x03\\x75\\x01\\x81\\x02\\x95\\x01\\x75\\x05\\x81\\x03\\x05\\x01\\x09\\x30\\x09\\x31\\x15\\x81\\x25\\x7f\\x75\\x08\\x95\\x02\\x81\\x06\\xc0\\xc0 > functions/hid.$N/report_desc
 C=1
 mkdir -p configs/c.$C/strings/0x409
 echo "Config $C: ECM network" > configs/c.$C/strings/0x409/configuration 
